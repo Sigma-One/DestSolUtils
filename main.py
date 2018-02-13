16cb5e6fc71bcd976c8f2b61513162497fc372ea
Adrijaned <adrijaned@gmail.com>
2018-02-13 21:02:14 +0100

    All hopefully done. It should work. (Not tested)

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ hull_json_descriptions = {"size": "How big do you want your ship to be (float)",
                           "price": "Price of the ship (int)",
                           "hirePrice": "Price of the ship to hire (int)",
                           "gunSlots": "Gun slots on the ship",
-                          "particleEmitters": ""}
+                          "particleEmitters": "ParticleEmitters on your ship"}
 b2d_file = {}
 b2d_json_label_text = "Open b2d file to use for physics,\nnone is loaded now"
 
@@ -50,6 +50,14 @@ class Application(tkinter.Frame):
         self.gunSlots_isUnderneathHull = tkinter.BooleanVar()
         self.gunSlots_allowsRotation = tkinter.BooleanVar()
         self.gunSlots_entry_selected = 0
+        self.particleEmitter_angleOffset = tkinter.StringVar()
+        self.particleEmitter_effectFile = tkinter.StringVar()
+        self.particleEmitter_size = tkinter.StringVar()
+        self.particleEmitter_tex = tkinter.StringVar()
+        self.particleEmitter_tint = tkinter.StringVar()
+        self.particleEmitter_floatsUp = tkinter.BooleanVar()
+        self.particleEmitter_trigger = tkinter.StringVar()
+        self.particleEmitter_hasLight = tkinter.BooleanVar()
 
     def configure_ability_on_load(self):
         if "type" in hull_json["ability"]:
@@ -99,8 +107,122 @@ class Application(tkinter.Frame):
         self.posarrays_widgets(right_column, "doorPos", 5)
 
         self.gunSlots_widgets(right_column)
+        self.particleEmitters_widgets(right_column)
 
     # right column items
+
+    # particleEmitters
+    def particleEmitters_widgets(self, column):
+        frame = tkinter.Frame(column, width=400, borderwidth=1, relief="ridge")
+        frame.grid(row=7, column=0, sticky=sticky_we, padx=1, pady=1)
+        tkinter.Label(frame, text=hull_json_descriptions["particleEmitters"]).grid(row=0, column=0)
+        tkinter.Button(frame, text="...", command=self.particleEmitters_window).grid(row=0, column=1)
+
+    def particleEmitters_window(self):
+        if self.temp_window is not None:
+            return
+        self.temp_window = tkinter.Toplevel(self, width=400)
+        self.temp_window.protocol('WM_DELETE_WINDOW', self.gunSlots_window_exit)
+        self.temp_window.attributes('-topmost', 'true')
+        frame_top = tkinter.Frame(self.temp_window)
+        frame_top.grid(row=0, column=0, pady=5)
+        frame_bottom = tkinter.Frame(self.temp_window)
+        frame_bottom.grid(row=1, column=0, pady=5)
+        self.listbox = tkinter.Listbox(frame_top)
+        self.listbox.grid(row=0, column=0, padx=5)
+        frame_top_right = tkinter.Frame(frame_top)
+        frame_top_right.grid(row=0, column=1, padx=5)
+        frame_temp = tkinter.Frame(frame_top_right, width=400, borderwidth=1, relief="ridge")
+        frame_temp.grid(row=0, column=0, sticky=sticky_we, padx=1, pady=1)
+        tkinter.Label(frame_temp, text="Position").grid(row=0, column=0)
+        tkinter.Entry(frame_temp, textvariable=self.tempPosString, width=10).grid(row=0, column=1)
+        frame_temp = tkinter.Frame(frame_top_right, width=400, borderwidth=1, relief="ridge")
+        frame_temp.grid(row=1, column=0, sticky=sticky_we, padx=1, pady=1)
+        tkinter.Label(frame_temp, text="trigger").grid(row=0, column=0)
+        tkinter.Entry(frame_temp, textvariable=self.particleEmitter_trigger, width=10).grid(row=0, column=1)
+        frame_temp = tkinter.Frame(frame_top_right, width=400, borderwidth=1, relief="ridge")
+        frame_temp.grid(row=2, column=0, sticky=sticky_we, padx=1, pady=1)
+        tkinter.Label(frame_temp, text="angleOffset").grid(row=0, column=0)
+        tkinter.Entry(frame_temp, textvariable=self.particleEmitter_angleOffset, width=10).grid(row=0, column=1)
+        frame_temp = tkinter.Frame(frame_top_right, width=400, borderwidth=1, relief="ridge")
+        frame_temp.grid(row=3, column=0, sticky=sticky_we, padx=1, pady=1)
+        tkinter.Checkbutton(frame_temp, text="hasLight", variable=self.particleEmitter_hasLight).grid(row=0, column=0)
+        frame_particle_def = tkinter.Frame(frame_top_right, width=400, borderwidth=1, relief="ridge")
+        frame_particle_def.grid(row=3, column=0, sticky=sticky_we, padx=1, pady=1, ipadx=5, ipady=5)
+        tkinter.Label(frame_particle_def, text="Particle thingies:").grid(row=0, column=0)
+        frame_temp = tkinter.Frame(frame_particle_def, width=400, borderwidth=1, relief="ridge")
+        frame_temp.grid(row=1, column=0, sticky=sticky_we, padx=1, pady=1)
+        tkinter.Label(frame_temp, text="effectFile").grid(row=0, column=0)
+        tkinter.Entry(frame_temp, textvariable=self.particleEmitter_effectFile, width=10).grid(row=0, column=1)
+        frame_temp = tkinter.Frame(frame_particle_def, width=400, borderwidth=1, relief="ridge")
+        frame_temp.grid(row=2, column=0, sticky=sticky_we, padx=1, pady=1)
+        tkinter.Label(frame_temp, text="size").grid(row=0, column=0)
+        tkinter.Entry(frame_temp, textvariable=self.particleEmitter_size, width=10).grid(row=0, column=1)
+        frame_temp = tkinter.Frame(frame_particle_def, width=400, borderwidth=1, relief="ridge")
+        frame_temp.grid(row=3, column=0, sticky=sticky_we, padx=1, pady=1)
+        tkinter.Label(frame_temp, text="tex").grid(row=0, column=0)
+        tkinter.Entry(frame_temp, textvariable=self.particleEmitter_tex, width=10).grid(row=0, column=1)
+        frame_temp = tkinter.Frame(frame_particle_def, width=400, borderwidth=1, relief="ridge")
+        frame_temp.grid(row=4, column=0, sticky=sticky_we, padx=1, pady=1)
+        tkinter.Label(frame_temp, text="tint").grid(row=0, column=0)
+        tkinter.Entry(frame_temp, textvariable=self.particleEmitter_tint, width=10).grid(row=0, column=1)
+        frame_temp = tkinter.Frame(frame_particle_def, width=400, borderwidth=1, relief="ridge")
+        frame_temp.grid(row=4, column=0, sticky=sticky_we, padx=1, pady=1)
+        tkinter.Checkbutton(frame_temp, text="floatsUp", ).grid(row=0, column=0)
+        frame_top_right = tkinter.Frame(frame_top_right)
+        frame_top_right.grid(row=4, column=0)
+        tkinter.Button(frame_top_right, command=self.particleEmitters_save, text="Save").grid(row=0, column=0, padx=3)
+        tkinter.Button(frame_top_right, command=self.particleEmitters_load, text="Select").grid(row=0, column=1)
+        tkinter.Button(frame_bottom, command=lambda: self.listbox.insert(tkinter.END, "0.5 0.5"), text="Add").grid(row=0, column=0)
+        tkinter.Button(frame_bottom, command=lambda: self.listbox.delete(tkinter.ACTIVE), text="Remove").grid(row=0, column=1)
+        tkinter.Button(frame_bottom, command=self.particleEmitters_window_exit, text="Exit").grid(row=0, column=2)
+
+    def particleEmitters_save(self):
+        if self.listbox.size() == 0:
+            return
+        hull_json["particleEmitters"][self.particleEmitters_entry_selected] = {
+            "position": self.tempPosString.get(),
+            "trigger": self.particleEmitter_trigger.get(),
+            "angleOffset": self.particleEmitter_angleOffset.get(),
+            "hasLight": self.particleEmitter_hasLight.get(),
+            "particle": {
+                "effectFile": self.particleEmitter_effectFile.get(),
+                "size": self.particleEmitter_size.get(),
+                "tex": self.particleEmitter_tex.get(),
+                "floatsUp": self.particleEmitter_floatsUp.get(),
+                "tint": self.particleEmitter_tint.get()
+        }}
+        self.listbox.delete(self.gunSlots_entry_selected)
+        self.listbox.insert(self.gunSlots_entry_selected, self.tempPosString.get())
+
+    def particleEmitters_load(self):
+        if self.listbox.size() == 0:
+            return
+        self.particleEmitters_entry_selected = self.listbox.index(tkinter.ACTIVE)
+        while self.particleEmitters_entry_selected + 1 > len(hull_json["particleEmitters"]):
+            hull_json["particleEmitters"].append({"position": "0.5 0.5", "trigger": "engine", "angleOffset": "0.0", "hasLight": False,
+                                          "particle": {
+                                              "effectFile": "FILE",
+                                              "size": "0",
+                                              "tex": "TEXTURE",
+                                              "floatsUp": False,
+                                              "tint": "TINT"
+                                          }})
+        item = hull_json["particleEmitters"][self.particleEmitters_entry_selected]
+        self.tempPosString.set(item["position"])
+        self.particleEmitter_trigger.set(item["trigger"])
+        self.particleEmitter_angleOffset.set(item["angleOffset"])
+        self.particleEmitter_hasLight.set(item["hasLight"])
+        self.particleEmitter_effectFile.set(item["particle"]["effectFile"])
+        self.particleEmitter_size.set(item["particle"]["size"])
+        self.particleEmitter_tex.set(item["particle"]["tex"])
+        self.particleEmitter_floatsUp.set(item["particle"]["floatsUp"])
+        self.particleEmitter_tint.set(item["particle"]["tint"])
+
+    def particleEmitters_window_exit(self):
+        self.listbox.destroy()
+        self.temp_window.destroy()
+        self.temp_window = None
 
     # gunSlots
     def gunSlots_widgets(self, column):
@@ -108,6 +230,7 @@ class Application(tkinter.Frame):
         frame.grid(row=6, column=0, sticky=sticky_we, padx=1, pady=1)
         tkinter.Label(frame, text=hull_json_descriptions["gunSlots"]).grid(row=0, column=0)
         tkinter.Button(frame, text="...", command=self.gunSlots_window).grid(row=0, column=1)
+
 
     def gunSlots_window(self):
         if self.temp_window is not None:
